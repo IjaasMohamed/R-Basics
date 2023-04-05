e5d21db4131885558d54589f2ea40508ed3ec569
IjaasMohamed <undyin0@gmail.com>
2023-04-05 04:10:27 -0300

    dataframes

---
+++ dataframes.R
@@ -0,0 +1,9 @@
+#dataframes - two dimensional heterogenous datastructure in the form of rows
+
+data.frame(fruit_name = c("Apple", "Banana", "Orange"), Fruit_cost=c(100,200,300)) -> fruit
+
+View(fruit)
+
+#extracting 
+
+fruit$fruit_name
